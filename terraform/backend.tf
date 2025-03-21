7c19189122b86be1304af904b10c4a62ddecf4fc
Ashiph <ashiphs@cybage.com>
2025-03-21 10:57:23 +0530

    updated bucket in tf backend, testin tf in nikita account

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "zenimax-terraform-state"
+    bucket         = "zenimax-terraform-state-1"
     key            = "terraform.tfstate"
     region         = "us-east-1"
 
